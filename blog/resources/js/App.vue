af3d2329ca1e8fba43a0521f9e0ad8c0e68b4296
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-06-08 23:16:43 +0300

    Connect Vue to the app

---
+++ blog/resources/js/App.vue
@@ -0,0 +1,108 @@
+<template>
+   <div id="wrapper">
+          <!-- header -->
+          <header class="header">
+            <div class="header-text">
+              <h1>Alphayo Blog</h1>
+              <h4>Home of verified news...</h4>
+            </div>
+            <div class="overlay"></div>
+          </header>
+
+          <!-- sidebar -->
+          <div class="sidebar">
+            <span class="closeButton">&times;</span>
+            <p class="brand-title"><a href="">Alphayo Blog</a></p>
+
+            <div class="side-links">
+              <ul>
+                <li><a class="active" href="index.html">Home</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+            </div>
+
+            <!-- sidebar footer -->
+            <footer class="sidebar-footer">
+              <div>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+              </div>
+
+              <small>&copy 2021 Alphayo Blog</small>
+            </footer>
+          </div>
+          <!-- Menu Button -->
+          <div class="menuButton">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+          </div>
+          <!-- main -->
+          <main class="container">
+            <h2 class="header-title">Latest Blog Posts</h2>
+            <section class="cards-blog latest-blog">
+              <div class="card-blog-content">
+                <img src="images/pic1.jpg" alt="" />
+                <p>
+                  2 hours ago
+                  <span style="float: right">Written By Natasha</span>
+                </p>
+                <h4 style="font-weight: bolder">
+                  <a href="single-blog.html"
+                    >Benefits of Getting Covid 19 Vaccination</a
+                  >
+                </h4>
+              </div>
+
+              <div class="card-blog-content">
+                <img src="images/pic2.jpg" alt="" />
+                <p>
+                  23 hours ago
+                  <span style="float: right">Written By Natasha</span>
+                </p>
+                <h4 style="font-weight: bolder">
+                  <a href="single-blog.html">Top 10 Music Stories Never Told</a>
+                </h4>
+              </div>
+
+              <div class="card-blog-content">
+                <img src="images/pic3.jpg" alt="" />
+                <p>
+                  2 days ago
+                  <span style="float: right">Written By Natasha</span>
+                </p>
+                <h4 style="font-weight: bolder">
+                  <a href="single-blog.html"
+                    >WRC Safari Rally Back To Kenya After 19 Years</a
+                  >
+                </h4>
+              </div>
+
+              <div class="card-blog-content">
+                <img src="images/pic4.jpg" alt="" />
+                <p>
+                  3 days ago
+                  <span style="float: right">Written By Natasha</span>
+                </p>
+                <h4 style="font-weight: bolder">
+                  <a href="single-blog.html">Premier League 2021/2022 Fixtures</a>
+                </h4>
+              </div>
+            </section>
+          </main>
+
+          <!-- Main footer -->
+          <footer class="main-footer">
+            <div>
+              <a href=""><i class="fab fa-facebook-f"></i></a>
+              <a href=""><i class="fab fa-instagram"></i></a>
+              <a href=""><i class="fab fa-twitter"></i></a>
+            </div>
+            <small>&copy 2023 Natasha</small>
+          </footer>
+        </div>
+
+</template>
--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -8,10 +8,17 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        
+        <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        />
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        @vite('resources/js/app.js')
 
     </head>
     <body class="antialiased">
-     
+       <div id="app"></div>
     </body>
 </html>
