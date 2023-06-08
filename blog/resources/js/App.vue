8ab5eb8f8984495b88d462fe1e0b07dd65238966
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-06-09 00:07:14 +0300

    Add page files

--- blog/resources/js/App.vue
+++ blog/resources/js/App.vue
@@ -3,7 +3,7 @@
           <!-- header -->
           <header class="header">
             <div class="header-text">
-              <h1>Alphayo Blog</h1>
+              <h1>The BlogHive</h1>
               <h4>Home of verified news...</h4>
             </div>
             <div class="overlay"></div>
@@ -12,7 +12,7 @@
           <!-- sidebar -->
           <div class="sidebar">
             <span class="closeButton">&times;</span>
-            <p class="brand-title"><a href="">Alphayo Blog</a></p>
+            <p class="brand-title"><a href="">BlogHive</a></p>
 
             <div class="side-links">
               <ul>
@@ -31,7 +31,7 @@
                 <a href=""><i class="fab fa-twitter"></i></a>
               </div>
 
-              <small>&copy 2021 Alphayo Blog</small>
+              <small>2021 Natasha Blog</small>
             </footer>
           </div>
           <!-- Menu Button -->
@@ -42,56 +42,7 @@
           </div>
           <!-- main -->
           <main class="container">
-            <h2 class="header-title">Latest Blog Posts</h2>
-            <section class="cards-blog latest-blog">
-              <div class="card-blog-content">
-                <img src="images/pic1.jpg" alt="" />
-                <p>
-                  2 hours ago
-                  <span style="float: right">Written By Natasha</span>
-                </p>
-                <h4 style="font-weight: bolder">
-                  <a href="single-blog.html"
-                    >Benefits of Getting Covid 19 Vaccination</a
-                  >
-                </h4>
-              </div>
-
-              <div class="card-blog-content">
-                <img src="images/pic2.jpg" alt="" />
-                <p>
-                  23 hours ago
-                  <span style="float: right">Written By Natasha</span>
-                </p>
-                <h4 style="font-weight: bolder">
-                  <a href="single-blog.html">Top 10 Music Stories Never Told</a>
-                </h4>
-              </div>
-
-              <div class="card-blog-content">
-                <img src="images/pic3.jpg" alt="" />
-                <p>
-                  2 days ago
-                  <span style="float: right">Written By Natasha</span>
-                </p>
-                <h4 style="font-weight: bolder">
-                  <a href="single-blog.html"
-                    >WRC Safari Rally Back To Kenya After 19 Years</a
-                  >
-                </h4>
-              </div>
-
-              <div class="card-blog-content">
-                <img src="images/pic4.jpg" alt="" />
-                <p>
-                  3 days ago
-                  <span style="float: right">Written By Natasha</span>
-                </p>
-                <h4 style="font-weight: bolder">
-                  <a href="single-blog.html">Premier League 2021/2022 Fixtures</a>
-                </h4>
-              </div>
-            </section>
+            <!-- CHANGES WITH PAGE -->
           </main>
 
           <!-- Main footer -->
@@ -101,7 +52,7 @@
               <a href=""><i class="fab fa-instagram"></i></a>
               <a href=""><i class="fab fa-twitter"></i></a>
             </div>
-            <small>&copy 2023 Natasha</small>
+            <small> 2023 Natasha</small>
           </footer>
         </div>
 
--- blog/resources/js/app.js
+++ blog/resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import {createApp}  from 'vue';
 import App from '../js/App.vue';
+import '../css/app.css';
 
 createApp(App).mount('#app')
